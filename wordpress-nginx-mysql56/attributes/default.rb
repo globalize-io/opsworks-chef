048cf4ed1727175ac99f44537ddad579e0ecb33b
Michael Berkovich <michael@translationexchange.com>
2017-06-09 16:51:23 -0700

    added Drupal support

---
+++ wordpress-nginx-mysql56/attributes/default.rb
@@ -0,0 +1,9 @@
+# MySQL settings
+default['mysql']['root_password'] = 'J9pe}z%s]2bUwaMJw6;ZaKLmRfZPeNDsB7u2jkMEW'
+default['mysql']['user_name']     = 'wpuser'
+default['mysql']['user_password'] = 'bkW.2fNcaY#d99zpvnQhE8kqbncfg*qi9jBozt(aG'
+default['mysql']['database_name'] = 'wordpress'
+
+# WP HTTP_ROOT
+default['drupal']['path'] = '/var/www/drupal/'
+
