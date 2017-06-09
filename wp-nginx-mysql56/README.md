a3290b63f5a7bd8acb0b71932026d372d2b5a4a3
Michael Berkovich <michael@translationexchange.com>
2017-06-09 15:58:54 -0700

    initial release

---
+++ wp-nginx-mysql56/README.md
@@ -0,0 +1,18 @@
+Description
+===========
+Installs Nginx, php5-fpm, MysQL and WordPress
+
+
+Requirements
+============
+Chef
+
+
+Attributes
+==========
+User, password for MySQL
+
+
+Usage
+=====
+
---
+++ wp-nginx-mysql56/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       'Translation Exchange'
+maintainer_email 'support@translationexchange.com'
+license          'All rights reserved'
+description      'Configures the latest WordPress with NGINX and MySQL'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.2'
